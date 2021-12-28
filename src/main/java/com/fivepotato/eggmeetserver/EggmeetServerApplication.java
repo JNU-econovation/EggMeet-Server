1d2ff3b1003d5811fe7782cb43a1061ddb51ef21
Kwon770 <sckwon770@gmail.com>
2021-12-29 02:43:19 +0900

    chore: init

---
+++ src/main/java/com/fivepotato/eggmeetserver/EggmeetServerApplication.java
@@ -0,0 +1,13 @@
+package com.fivepotato.eggmeetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggmeetServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EggmeetServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fivepotato/eggmeetserver/EggmeetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivepotato.eggmeetserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggmeetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
