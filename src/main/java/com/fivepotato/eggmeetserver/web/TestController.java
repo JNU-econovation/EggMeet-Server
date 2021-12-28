2fa55b3378f4fa60f924a5fc04b0ba6509d3fcc2
Kwon770 <sckwon770@gmail.com>
2021-12-29 03:09:17 +0900

    feat: Create basic structure

---
+++ src/main/java/com/fivepotato/eggmeetserver/web/TestController.java
@@ -0,0 +1,13 @@
+package com.fivepotato.eggmeetserver.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String greet() {
+        return "hello";
+    }
+}
