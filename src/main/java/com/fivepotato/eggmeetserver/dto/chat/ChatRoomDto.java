d6058c6b151ccbbb90cdbc96c39eb87ce19ac253
Kwon770 <sckwon770@gmail.com>
2022-01-15 22:53:08 +0900

    fix: Add NoArgsConstructor in ChatRoomDto to fix JSON parse bug

--- src/main/java/com/fivepotato/eggmeetserver/dto/chat/ChatRoomDto.java
+++ src/main/java/com/fivepotato/eggmeetserver/dto/chat/ChatRoomDto.java
@@ -2,12 +2,14 @@ package com.fivepotato.eggmeetserver.dto.chat;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+@NoArgsConstructor
 @Getter
 public class ChatRoomDto {
 
