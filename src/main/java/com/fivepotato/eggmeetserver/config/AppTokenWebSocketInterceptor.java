088c5849e6c6813876ed1682382a32f365e00c66
Kwon770 <sckwon770@gmail.com>
2022-01-26 01:57:45 +0900

    ...

--- src/main/java/com/fivepotato/eggmeetserver/config/AppTokenWebSocketInterceptor.java
+++ src/main/java/com/fivepotato/eggmeetserver/config/AppTokenWebSocketInterceptor.java
@@ -26,9 +26,27 @@ public class AppTokenWebSocketInterceptor implements ChannelInterceptor {
         log.info(message.toString());
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             String jwt = parseTokenFromWebSocketHeader(accessor);
+            log.info(jwt);
             appTokenProvider.validateToken(jwt);
         }
 
         return message;
     }
+
+//    if (StompCommand.CONNECT == cmd || StompCommand.SEND == cmd) {
+//        Authentication authenticatedUser = null;
+//        String authorization = accessor.getFirstNativeHeader("Authorization:");
+//        String credentialsToDecode = authorization.split("\\s")[1];
+//        String credentialsDecoded = StringUtils.newStringUtf8(Base64.decodeBase64(credentialsToDecode));
+//        String[] credentialsDecodedSplit = credentialsDecoded.split(":");
+//        final String username = credentialsDecodedSplit[0];
+//        final String password = credentialsDecodedSplit[1];
+//        authenticatedUser = userAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+//        if (authenticatedUser == null) {
+//            throw new AccessDeniedException();
+//        }
+//        SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
+//        accessor.setUser(authenticatedUser);
+//    }
+//        return message;
 }
