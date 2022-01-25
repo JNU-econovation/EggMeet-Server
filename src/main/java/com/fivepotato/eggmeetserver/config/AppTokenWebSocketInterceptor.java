4832a5817b236d296f8a08c8bccc6606c8a03f83
Kwon770 <sckwon770@gmail.com>
2022-01-26 04:13:34 +0900

    ...

--- src/main/java/com/fivepotato/eggmeetserver/config/AppTokenWebSocketInterceptor.java
+++ src/main/java/com/fivepotato/eggmeetserver/config/AppTokenWebSocketInterceptor.java
@@ -28,9 +28,9 @@ public class AppTokenWebSocketInterceptor implements ChannelInterceptor {
 //        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
         StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
         log.info("[MESSAGE] : " + message);
-        if (StompCommand.CONNECT.equals(accessor.getCommand()) ||
-                StompCommand.SEND.equals(accessor.getCommand())) {
-//        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+//        if (StompCommand.CONNECT.equals(accessor.getCommand()) ||
+//                StompCommand.SEND.equals(accessor.getCommand())) {
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             String jwt = parseTokenFromWebSocketHeader(accessor);
             appTokenProvider.validateToken(jwt);
 
